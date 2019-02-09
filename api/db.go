c7c4f9be6a59a9024f312d4f0562a846619f52c2
steveoc <steveoc64@gmail.com>
2019-02-10 01:59:46 +1000

    get the data from the DB now

---
+++ api/db.go
@@ -0,0 +1,79 @@
+package api
+
+import (
+	"database/sql"
+	"os"
+	"time"
+
+	dat "github.com/helloeave/dat/dat"
+	runner "github.com/helloeave/dat/sqlx-runner"
+	"github.com/sirupsen/logrus"
+)
+
+type DB interface {
+	StockReport() (*stockReport, error)
+}
+
+type stock struct {
+	Brand string `db:"brand"`
+	Item  string `db:"id"`
+	Descr string `db:"name"`
+	Qty   string `db:"qty"`
+}
+
+type stockReport struct {
+	Note  string
+	Items []*stock
+}
+
+type pgDB struct {
+	log *logrus.Logger
+	db  *runner.DB
+}
+
+// StockReport retuns a struture with all the stock data needed
+func (h *pgDB) StockReport() (*stockReport, error) {
+	retval := &stockReport{}
+
+	// Get the overall status
+	err := h.db.Select("descr").From("brandStockStatus").Limit(1).QueryScalar(&retval.Note)
+	if err != nil {
+		return retval, err
+	}
+
+	// Get the items part
+	err = h.db.Select("brand,id,name,round(random()*100) as qty").From("items").QueryStructs(&retval.Items)
+	return retval, err
+}
+
+// newDB is a private function to setup the database connection
+func newDB(l *logrus.Logger) *pgDB {
+
+	// create a normal database connection through database/sql
+	db, err := sql.Open("postgres", os.Getenv("API_DSN"))
+	if err != nil {
+		l.WithError(err).Fatal("could not connect to DB")
+	}
+
+	// ensures the database can be pinged with an exponential backoff (15 min)
+	runner.MustPing(db)
+
+	// set to reasonable values for production
+	db.SetMaxIdleConns(4)
+	db.SetMaxOpenConns(16)
+
+	// set this to enable interpolation
+	dat.EnableInterpolation = true
+
+	// set to check things like sessions closing.
+	// Should be disabled in production/release builds.
+	dat.Strict = false
+
+	// Log any query over 10ms as warnings. (optional)
+	runner.LogQueriesThreshold = 10 * time.Millisecond
+	l.Info("Connected to DB")
+	return &pgDB{
+		log: l,
+		db:  runner.NewDB(db, "postgres"),
+	}
+}
--- api/http_api.go
+++ api/http_api.go
@@ -3,18 +3,21 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
+	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 )
 
 // Http is an implementation of the API over HTTP
 type Http struct {
 	log *logrus.Logger
+	db  *pgDB
 }
 
 // NewHttp returns a new HttpAPI
 func NewHttp(l *logrus.Logger) *Http {
-	return &Http{log: l}
+	return &Http{log: l, db: newDB(l)}
 }
 
 // Version returns the current version number as a string
@@ -22,57 +25,44 @@ func (h *Http) Version() string {
 	return "v1"
 }
 
-func (h *Http) Log(r *http.Request, txt string) {
+func (h *Http) Log(r *http.Request, t time.Time, txt string) {
 	h.log.WithFields(logrus.Fields{
-		"addr":   r.RemoteAddr,
-		"proto":  r.Proto,
-		"method": r.Method,
-		"URI":    r.RequestURI,
+		"Duration": time.Since(t).String(),
+		"addr":     r.RemoteAddr,
+		"proto":    r.Proto,
+		"method":   r.Method,
+		"URI":      r.RequestURI,
 	}).Println(txt)
 }
 
 // Default does not much at all yet
 func (h *Http) Default(w http.ResponseWriter, r *http.Request) {
+	t1 := time.Now()
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Nothing to see here\n"))
-	h.Log(r, "Default")
+	h.Log(r, t1, "Default")
 }
 
 // Hello writes hello world as a response
 func (h *Http) Hello(w http.ResponseWriter, r *http.Request) {
+	t1 := time.Now()
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello, World!" + "\n"))
-	h.Log(r, "Hello")
+	h.Log(r, t1, "Hello")
 }
 
 // Stock writes a stock update in JSON
 func (h *Http) Stock(w http.ResponseWriter, r *http.Request) {
+	t1 := time.Now()
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	type stock struct {
-		Item  string
-		Descr string
-		Qty   string
+	items, err := h.db.StockReport()
+	if err != nil {
+		h.log.WithError(err).Warn("error fetching all the items")
 	}
-
-	type stocks struct {
-		Note  string
-		Items []stock
-	}
-
-	// TODO - get this from the DB
-	s := stocks{
-		Note: "We have plenty left in stock",
-		Items: []stock{
-			stock{Item: "abc", Descr: "the abc item", Qty: "10"},
-			stock{Item: "def", Descr: "the def item", Qty: "20"},
-			stock{Item: "xyz", Descr: "the xyz item", Qty: "6"},
-		},
-	}
-
-	json.NewEncoder(w).Encode(s)
-	h.Log(r, "Stock")
+	json.NewEncoder(w).Encode(items)
+	h.Log(r, t1, "Stock")
 }
