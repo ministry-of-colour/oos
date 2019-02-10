6e49a48e908b2f4b3c90e10c9dc08092f5545cd2
steveoc <steveoc64@gmail.com>
2019-02-10 22:58:02 +1000

    interim end of weekend update

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(VueRouter)
+Vue.use(Vuetify)
+const routes = [
+  {path: '/', component: App}
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
